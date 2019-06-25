1d76d06e75a460017c99ad663f630a23dfd79990
Shivakumar <shivakumar@mavenhive.in>
2019-06-25 18:39:47 +0530

    Add bunny

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in mailer.gemspec
 gemspec
+gem "bunny", ">= 2.14.1"
--- lib/mailer.rb
+++ lib/mailer.rb
@@ -1,5 +1,20 @@
-require "mailer/version"
+require "rubygems"
+require "bunny"
 
-module Mailer
-  # Your code goes here...
+STDOUT.sync = true
+
+conn = Bunny.new
+conn.start
+
+ch = conn.create_channel
+q  = ch.queue("bunny.examples.hello_world_2", :auto_delete => false)
+x  = ch.fanout("aus.eng")
+
+q.subscribe do |delivery_info, metadata, payload|
+  puts "Received #{payload}"
 end
+
+x.publish("Hello bang!", :routing_key => q.name)
+
+sleep 1.0
+conn.close
